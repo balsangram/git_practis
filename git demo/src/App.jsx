a4b743c1a238c14de4b0b6faa6ce902f2fc00c9e
sangram <balsangram@gmail.com>
2025-06-15 19:37:17 +0530

    Fix login bug

--- git demo/src/App.jsx
+++ git demo/src/App.jsx
@@ -1,11 +1,18 @@
+import FifthPage from "./page/FifthPage";
 import FirstPage from "./page/FirstPage";
+import FourthPage from "./page/FourthPage";
 import SecondPage from "./page/secondPage";
+import ThardPage from "./page/ThardPage";
 
 function App() {
   return (
     <>
+    <h1>hello sangram</h1>
       <FirstPage />
       <SecondPage />
+      <ThardPage />
+      <FourthPage />
+      <FifthPage />
     </>
   );
 }
